5876543f9f0d43c9bcefbdfd963553b5e75ecd69
IamKashifKhan <55381045+IamKashifKhan@users.noreply.github.com>
2024-02-12 02:38:55 -0400

    Initial Commit

---
+++ Assignment/Assignment/AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Assignment.ViewModels;
+using Assignment.Views;
+using Xamarin.Forms;
+
+namespace Assignment
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
+
---
+++ Assignment/Assignment/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Assignment.ViewModels;
+
+namespace Assignment.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
